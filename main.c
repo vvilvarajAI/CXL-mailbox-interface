1692f15bdcb6c2a4fafb7eb0f42dfe4ac28031c4
Vivek Vilvaraj <vivek.vilvaraj@gmail.com>
2024-03-12 18:32:33 +0000

    sucessfully get mailbox address

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <sys/mman.h>
 #include <pci/pci.h>
 #include "main.h"
 
 #define CXL_Vendor_ID 0x1E98
+#define CXL_DEVICE_REGISTERS_ID 0x03
 
 int main(int argc, char *argv[])
 {
@@ -51,33 +54,38 @@ int main(int argc, char *argv[])
     }
     printf("Device: Vendor 0x%04x, Device 0x%04x\n", pdev->vendor_id, pdev->device_id);
     printf("Device Class: 0x%04x\n", pdev->device_class);
+    print_config_header(pdev);
 #ifdef DEBUG
     print_extended_config(pdev);
 #endif
+    uint16_t register_locator_offset = get_dvsec_register_locator_offset(pdev);
+    printf("register Locator header Offset: 0x%04x\n", register_locator_offset);
 
-    uint32_t ext_cap_off = 0x100;
-    uint16_t ext_cap_off_val=0;
-    uint16_t val = pci_read_word(pdev,ext_cap_off+0x02);
-    printf("val: 0x%04x\n", val);
-    ext_cap_off_val = val>>4;
-    printf("ext_cap_off_val: 0x%04x\n", ext_cap_off_val);
+    uint32_t mailbox_base_address = get_mailbox_base_address(pdev);
+    printf("Mailbox Base Address: 0x%08x\n", mailbox_base_address);
 
-    PCIE_EXT_CAP_HDR pcie_ext_cap_hdr;
-    pci_read_block(pdev, ext_cap_off_val, &pcie_ext_cap_hdr, sizeof(pcie_ext_cap_hdr));
-    printf("PCIE_EXT_CAP_HDR: PCIE_ext_cap_ID: 0x%04x, Cap_Ver: 0x%04x, Next_Cap_ofs: 0x%04x\n", pcie_ext_cap_hdr.PCIE_ext_cap_ID, pcie_ext_cap_hdr.Cap_Ver, pcie_ext_cap_hdr.Next_Cap_ofs);
-    printf("DVSEC_HDR1: DVSEC_Vendor_ID: 0x%04x, DVSEC_Rev: 0x%04x, DVSEC_Length: 0x%04x\n", pcie_ext_cap_hdr.DVSEC_hdr1.DVSEC_Vendor_ID, pcie_ext_cap_hdr.DVSEC_hdr1.DVSEC_Rev, pcie_ext_cap_hdr.DVSEC_hdr1.DVSEC_Length);
-    printf("DVSEC_HDR2: DVSEC_ID: 0x%04x\n", pcie_ext_cap_hdr.DVSEC_hdr2.DVSEC_ID);
-    if (pcie_ext_cap_hdr.DVSEC_hdr1.DVSEC_Vendor_ID == CXL_Vendor_ID) {
-                #ifdef DEBUG
-                printf("DVSEC ID is %x\n", pcie_ext_cap_hdr.DVSEC_hdr2.DVSEC_ID);
-                #endif
-                if (pcie_ext_cap_hdr.DVSEC_hdr2.DVSEC_ID == 0x8) {
-                    printf("CXL Device found\n");
-                }
-            }
     pci_cleanup(pacc);
     return 0;
 }
+
+void print_config_header(struct pci_dev *pdev)
+{
+    printf("Configuration Header:\n");
+    PCIE_CONFIG_HDR pcie_config_hdr;
+    pci_read_block(pdev, 0, &pcie_config_hdr, sizeof(pcie_config_hdr));
+    printf("Vendor ID: 0x%04x\n", pcie_config_hdr.Vendor_ID);
+    printf("Device ID: 0x%04x\n", pcie_config_hdr.Device_ID);
+    printf("Command: 0x%04x\n", pcie_config_hdr.Command);
+    printf("Status: 0x%04x\n", pcie_config_hdr.Status);
+    printf("Rev ID: 0x%02x\n", pcie_config_hdr.Rev_ID);
+    printf("Class Code: 0x%06x\n", pcie_config_hdr.Class_Code);
+    printf("Misc: 0x%08x\n", pcie_config_hdr.Misc);
+    for (int i = 0; i < 6; i++)
+    {
+        printf("BAR %d: Locatable: 0x%02x, Prefetchable: 0x%02x, Base_Address: 0x%08x\n", i, pcie_config_hdr.Base_Address_Registers[i].Locatable, pcie_config_hdr.Base_Address_Registers[i].Prefetchable, pcie_config_hdr.Base_Address_Registers[i].Base_Address);
+    }
+}
+
 void print_extended_config(struct pci_dev *pdev)
 {
     // Print the configuration space
@@ -92,4 +100,108 @@ void print_extended_config(struct pci_dev *pdev)
             printf("\n");
         }
     }
+}
+
+uint16_t get_dvsec_register_locator_offset(struct pci_dev *pdev)
+{
+    uint16_t ext_cap_off_val = 0x100;
+
+    while (ext_cap_off_val != 0)
+    {
+        PCIE_EXT_CAP_HDR pcie_ext_cap_hdr;
+        pci_read_block(pdev, ext_cap_off_val, &pcie_ext_cap_hdr, sizeof(pcie_ext_cap_hdr));
+        printf("\nPCIE_EXT_CAP_HDR:\n PCIE_ext_cap_ID: 0x%04x, \n Cap_Ver: 0x%04x, \nNext_Cap_ofs: 0x%04x\n", pcie_ext_cap_hdr.PCIE_ext_cap_ID, pcie_ext_cap_hdr.Cap_Ver, pcie_ext_cap_hdr.Next_Cap_ofs);
+        printf("DVSEC_HDR1:\n DVSEC_Vendor_ID: 0x%04x,\n DVSEC_Rev: 0x%04x,\n DVSEC_Length: 0x%04x\n", pcie_ext_cap_hdr.DVSEC_hdr1.DVSEC_Vendor_ID, pcie_ext_cap_hdr.DVSEC_hdr1.DVSEC_Rev, pcie_ext_cap_hdr.DVSEC_hdr1.DVSEC_Length);
+        printf("DVSEC_HDR2:\n DVSEC_ID: 0x%04x\n\n", pcie_ext_cap_hdr.DVSEC_hdr2.DVSEC_ID);
+        if (pcie_ext_cap_hdr.DVSEC_hdr1.DVSEC_Vendor_ID == CXL_Vendor_ID)
+        {
+#ifdef DEBUG
+            printf("DVSEC ID is %x\n", pcie_ext_cap_hdr.DVSEC_hdr2.DVSEC_ID);
+#endif
+            if (pcie_ext_cap_hdr.DVSEC_hdr2.DVSEC_ID == 0x8)
+            {
+                printf("CXL Device found\n");
+                return ext_cap_off_val;
+            }
+        }
+
+        ext_cap_off_val = pcie_ext_cap_hdr.Next_Cap_ofs;
+    }
+    return 0;
+}
+
+uint32_t get_mailbox_base_address (struct pci_dev *pdev)
+{
+    uint32_t mailbox_base_address = 0;
+    uint32_t base_address = 0;
+    uint16_t register_locator_offset = get_dvsec_register_locator_offset(pdev);
+    PCIE_CONFIG_HDR pcie_config_hdr;
+    pci_read_block(pdev, 0, &pcie_config_hdr, sizeof(pcie_config_hdr));
+    if (register_locator_offset != 0)
+    {
+        registerLocator register_locator;
+        pci_read_block(pdev, register_locator_offset, &register_locator, sizeof(register_locator));
+
+        printf("Register Locator:\n");
+        printf("PCIE_ext_cap_hdr: PCIE_ext_cap_ID: 0x%04x, Cap_Ver: 0x%04x, Next_Cap_ofs: 0x%04x\n", register_locator.PCIE_ext_cap_hdr.PCIE_ext_cap_ID, register_locator.PCIE_ext_cap_hdr.Cap_Ver, register_locator.PCIE_ext_cap_hdr.Next_Cap_ofs);
+        uint32_t register_block_number = get_register_block_number_from_header(&register_locator);
+        for(int i=0;i<register_block_number;i++){
+            if(register_locator.Register_Block[i].Register_Offset_Low.Register_Block_Identifier != CXL_DEVICE_REGISTERS_ID){
+                continue;
+            }
+        printf("Register Block %d: Register_BIR: 0x%02x\n", i, register_locator.Register_Block[i].Register_Offset_Low.Register_BIR);
+        printf("Register Block %d: Register_Block_Identifier: 0x%02x\n", i, register_locator.Register_Block[i].Register_Offset_Low.Register_Block_Identifier);
+        printf("Register Block %d: Register_Block_Offset_Low: 0x%04x\n", i, register_locator.Register_Block[i].Register_Offset_Low.Register_Block_Offset_Low);
+        printf("Register Block %d: Register_Block_Offset_High: 0x%08x\n",i,  register_locator.Register_Block[i].Register_Offset_High.Register_Block_Offset_High);
+        
+        base_address = pcie_config_hdr.Base_Address_Registers[register_locator.Register_Block[i].Register_Offset_Low.Register_BIR].Base_Address << 4 |
+                                register_locator.Register_Block[i].Register_Offset_Low.Register_Block_Offset_Low << 16 |
+                                register_locator.Register_Block[i].Register_Offset_High.Register_Block_Offset_High << 32;
+        
+        printf("Register Block %d: Base Address: 0x%08x\n", i, base_address);
+        }
+    }
+    DEVICE_CAPABILITIES_ARRAY_REGISTER dev_cap_arr_reg;
+    MemoryDeviceRegisters mem_dev_reg;
+
+    int fd = open("/dev/mem", O_RDWR | O_DSYNC);
+    if (fd == -1) {
+        perror("Error opening /dev/mem");
+        exit(1);
+    }
+
+    void *map_base = mmap(NULL, 0x1000, PROT_READ, MAP_SHARED, fd, base_address);
+    if (map_base == MAP_FAILED) {
+        perror("Error mapping memory");
+        close(fd);
+        exit(1);
+    }
+    memcpy(&dev_cap_arr_reg, map_base, sizeof(dev_cap_arr_reg));
+    printf("Device Capabilities Array Register: Capability_ID: 0x%04x, Version: 0x%02x, Capabilities_Count: 0x%04x\n", dev_cap_arr_reg.Capability_ID, dev_cap_arr_reg.Version, dev_cap_arr_reg.Capabilities_Count);
+    if(dev_cap_arr_reg.Capability_ID == 0x0){
+        memcpy(&mem_dev_reg, map_base, sizeof(mem_dev_reg));
+        for(int i=0;i<3;i++){
+            printf("Device Capability Header %d: Capability_ID: 0x%04x, Version: 0x%02x, Offset: 0x%08x, Length: 0x%08x\n", i, mem_dev_reg.Device_Capability_Header[i].Capability_ID, mem_dev_reg.Device_Capability_Header[i].Version, mem_dev_reg.Device_Capability_Header[i].Offset, mem_dev_reg.Device_Capability_Header[i].Length);
+            if(mem_dev_reg.Device_Capability_Header[i].Capability_ID == 0x2){
+                printf("Offset = 0x%08x\n", mem_dev_reg.Device_Capability_Header[i].Offset);
+                printf("Length = 0x%08x\n", mem_dev_reg.Device_Capability_Header[i].Length);
+                mailbox_base_address = base_address + mem_dev_reg.Device_Capability_Header[i].Offset;
+                break;
+            }
+        }
+    }
+
+    if (munmap(map_base, 4096) == -1) {
+        perror("Error unmapping memory");
+        close(fd);
+        exit(1);
+    }
+
+    close(fd);
+        return mailbox_base_address;
+}
+
+uint32_t get_register_block_number_from_header(registerLocator *register_locator)
+{
+    return ((register_locator->PCIE_ext_cap_hdr.DVSEC_hdr1.DVSEC_Length -10-2)/8);
 }
--- main.h
+++ main.h
@@ -1,4 +1,30 @@
 #pragma once
+
+typedef struct PCIE_CLASS_CODE {
+    uint8_t Prog_if;
+    uint8_t Sub_Class_Code;  
+    uint8_t Base_Class_Code; 
+} PCIE_CLASS_CODE;
+
+typedef struct BAR {
+    uint32_t Region_Type    : 1;
+    uint32_t Locatable      : 2;
+    uint32_t Prefetchable   : 1;
+    uint32_t Base_Address   : 28;
+} BAR;
+
+typedef struct PCIE_CONFIG_HDR {
+    uint16_t Vendor_ID;
+    uint16_t Device_ID;
+    uint16_t Command;
+    int16_t Status;
+    uint8_t Rev_ID;
+    PCIE_CLASS_CODE Class_Code;
+    uint32_t Misc;
+    BAR Base_Address_Registers[6];
+    int32_t Misc2[6];
+} PCIE_CONFIG_HDR; 
+
 typedef struct {
     uint16_t DVSEC_Vendor_ID : 16;
     uint16_t DVSEC_Rev : 4;
@@ -17,5 +43,54 @@ typedef struct {
     DVSEC_HDR2 DVSEC_hdr2;
 } PCIE_EXT_CAP_HDR;
 
+typedef struct {
+    uint8_t Register_BIR : 3;
+    uint8_t RsvdP : 5;
+    uint8_t Register_Block_Identifier : 8;
+    uint16_t Register_Block_Offset_Low : 16;
+} REGISTER_OFFSET_LOW;
 
+typedef struct {
+    uint32_t Register_Block_Offset_High;
+} REGISTER_OFFSET_HIGH;
+
+typedef struct {
+    REGISTER_OFFSET_LOW Register_Offset_Low;
+    REGISTER_OFFSET_HIGH Register_Offset_High;
+} REGISTER_BLOCK;
+
+typedef struct {
+    PCIE_EXT_CAP_HDR PCIE_ext_cap_hdr;
+    uint16_t RsvdP;
+    REGISTER_BLOCK Register_Block[4];
+} registerLocator;
+
+typedef struct DEVICE_CAPABILITIES_ARRAY_REGISTER {
+    uint16_t Capability_ID;
+    uint8_t Version;
+    uint8_t Reserved;
+    uint16_t Capabilities_Count;
+    uint8_t Reserved2[10];
+} DEVICE_CAPABILITIES_ARRAY_REGISTER;
+
+typedef struct {
+    uint16_t Capability_ID;
+    uint8_t Version;
+    uint8_t Reserved;
+    uint32_t Offset;
+    uint32_t Length;
+    uint32_t Reserved2;
+} DEVICE_CAPABILITIES_HEADER;
+
+typedef struct {
+    DEVICE_CAPABILITIES_ARRAY_REGISTER Device_Capabilities_Array_Register; 
+    DEVICE_CAPABILITIES_HEADER Device_Capability_Header[3]; // Replace MAX_HEADERS with desired header count
+    uint8_t Device_Capability[4096-16-16*3];  // Replace MAX_DEVICE_CAPABILITY_SIZE
+} MemoryDeviceRegisters;
+
+
+void print_config_header(struct pci_dev *pdev);
 void print_extended_config(struct pci_dev *pdev);
+uint16_t get_dvsec_register_locator_offset(struct pci_dev *pdev);
+uint32_t get_mailbox_base_address (struct pci_dev *pdev);
+uint32_t get_register_block_number_from_header(registerLocator *register_locator);
